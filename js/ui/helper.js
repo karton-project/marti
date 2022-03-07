0272c62cf70f60ece468c1aed83d012f677cbcc1
alpay sabuncuoglu <asabuncuoglu13@ku.edu.tr>
2022-03-07 14:39:15 +0300

    tr-tutorial update

--- js/ui/helper.js
+++ js/ui/helper.js
@@ -19,7 +19,7 @@ function showList(inp, arr, val) {
             /*execute a function when someone clicks on the item value (DIV element):*/
             b.addEventListener("click", function(e) {
                 /*insert the value for the autocomplete text field:*/
-                inp.value += this.getElementsByTagName("input")[0].value;
+                (inp.value.includes(',')) ? inp.value += this.getElementsByTagName("input")[0].value : inp.value = this.getElementsByTagName("input")[0].value;
                 /*close the list of autocompleted values,
                 (or any other open lists of autocompleted values:*/
                 closeAllLists(e.target);
